4f31eedc4f974ab9b6fbe01eed802a41dc7bd140
sixban6 <sixban6>
2026-03-02 00:21:58 +0800

    fix: support tailscale

--- internal/deploy/singbox.go
+++ internal/deploy/singbox.go
@@ -209,6 +209,7 @@ func (sbs *SingBoxServer) DeploySingbox() error {
 	}
 	sbs.ShareLinkHy2 = fmt.Sprintf("hysteria2://%s@%s/?sni=%s&alpn=h3&insecure=0#%s", sbs.hyUUID, sbs.cfg.Server.SBDomain, sbs.cfg.Server.SBDomain, sbs.hy2Tag)
 	sbs.ShareLinkVless = fmt.Sprintf("vless://%s@%s:443?type=tcp&encryption=none&security=reality&pbk=%s&fp=chrome&sni=www.microsoft.com&sid=%s&flow=xtls-rprx-vision#%s", sbs.hyUUID, sbs.cfg.Server.SBDomain, sbs.publicKey, sbs.shortID, sbs.vrTag)
+	sbs.ShowLoginInfo()
 	return nil
 }
 
